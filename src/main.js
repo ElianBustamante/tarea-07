3bcadd84990174a2a30e884d4973e707fb113f30
Elian Bustamante <e.bustamante02@ufromail.cl>
2022-06-26 16:36:39 -0400

    Corrección estructura proyecto vue, se agregan y reemplazan componentes, NewTodo, Todo, vista Home

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import 'bootstrap'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
+import router from './router'
 
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
